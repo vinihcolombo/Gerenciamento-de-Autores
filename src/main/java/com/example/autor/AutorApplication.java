13d03a7c2f6218bbe7e85647b5edb75dddfdbd86
flavioaugustto <flavioaugustosilv@gmail.com>
2026-03-23 09:20:11 -0300

    Organização de pastas

---
+++ src/main/java/com/example/autor/AutorApplication.java
@@ -0,0 +1,13 @@
+package com.example.autor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutorApplication.class, args);
+	}
+
+}
