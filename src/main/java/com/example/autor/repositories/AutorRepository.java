fafd635ed29a6711b096dd8e36ce8e6c4a4c98cb
flavioaugustto <flavioaugustosilv@gmail.com>
2026-03-23 09:32:32 -0300

    Repositorio

--- src/main/java/com/example/autor/repositories/AutorRepository.java
+++ src/main/java/com/example/autor/repositories/AutorRepository.java
@@ -1,4 +1,7 @@
-package com.example.autor.repositories;
+package com.example.Produto.repositories;
 
-public interface AutorRepository {
+import com.example.Produto.models.AutorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<AutorModel, Long> {
 }
